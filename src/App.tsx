ae2a5030152f654b88b246e3fd4922ddae2be267
KarimALI88 <karimalielsayed88@gmail.com>
2026-04-29 08:46:54 +0300

    auth context aand react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import '@/App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useAuth } from './context/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+const queryClient = new QueryClient();
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  return <>{children}</>;
+};  
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Smart Bundle Builder</h1>
-    </>
+    </QueryClientProvider>
   )
 }
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState } from "react";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  setIsAuthenticated: (value: boolean) => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(() => !!localStorage.getItem("token"));
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const ctx = useContext(AuthContext);
+  if (!ctx) throw new Error("useAuth must be used within AuthProvider");
+  return ctx;
+};
